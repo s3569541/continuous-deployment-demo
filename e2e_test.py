4c8b956e45ad0c1b861c4e437b715b0f97138cc6
Joseph Williams <s3569541@student.rmit.edu.au>
2019-09-07 12:00:30 +1000

    Adds encrypted credentials for Travis and updates project name

--- e2e_test.py
+++ e2e_test.py
@@ -11,7 +11,7 @@
 import urllib2
 import logging
 
-HOST='http://travis.appspot.com'
+HOST='http://learning-travis.appspot.com'
 
 # [START e2e]
 response = urllib2.urlopen("{}/get_author/ulysses".format(HOST))
